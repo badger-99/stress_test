60b06f1e000a3a1ff3fa56aee70e4193408430d6
Alfred M. <alpeter91@gmail.com>
2025-10-14 23:24:28 +0300

    Create header with theme toggle

---
+++ src/components/header.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import ThemeToggle from "./theme-toggle";
+
+export default function Header() {
+	return (
+		<header className='fixed w-full top-0 left-0 flex items-center justify-center overflow-hidden border border-b-2 z-50 bg-background'>
+			<div className='w-full'>
+				<div className='flex items-center justify-between px-4 py-4 sm:py-4'>
+					<div className='flex items-center space-x-2'>
+						<div className='w-10 h-10 rounded-lg bg-gradient-to-br from-blue-600 to-blue-400 flex items-center justify-center text-2xl font-bold text-white'>
+							ST
+						</div>
+						<span className='text-lg font-semibold select-none'>StressTest</span>
+					</div>
+
+					<ThemeToggle size={20} />
+				</div>
+			</div>
+		</header>
+	);
+}
