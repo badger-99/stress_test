dd522dafe962fcaf4b98bf1ca40e7f5a972a08b1
Alfred M. <alpeter91@gmail.com>
2025-10-14 23:24:59 +0300

    Add header to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/providers";
+import Header from "../components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +26,11 @@ export default function RootLayout({
 }>) {
   return (
 		<html lang='en' suppressHydrationWarning={true}>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<Providers>{children}</Providers>
+			<body className={`flex flex-col min-h-screen pt-19 ${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
 			</body>
 		</html>
 	);
