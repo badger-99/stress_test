ac7a1018b4ffa709d5ce741fbbf9d5d3477b4576
Alfred M. <alpeter91@gmail.com>
2025-10-15 22:54:08 +0300

    Fix redirect bug

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -4,14 +4,22 @@ import { useResults } from '@/providers/results-provider';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Report } from '@/components/report';
+import { useEffect } from 'react';
 
 export default function Results() {
   const router = useRouter();
   const { results } = useResults();
 
-  if (!results.latest || !results.history) {
-    router.push('/');
-  }
+  useEffect(() => {
+		if (!results.latest || !results.history) {
+			router.push('/');
+		}
+	}, [results, router]);
+
+	// If redirecting, avoid rendering rest of component
+	if (!results.latest || !results.history) {
+		return null;
+	}
 
   const latest = results.latest!
 
