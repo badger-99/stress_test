801386e0a101f63b3ac9ab2b5d67f44330783913
Alfred M. <alpeter91@gmail.com>
2025-10-16 15:36:42 +0300

    Add error page

---
+++ src/app/error/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function ErrorPage() {
+	return (
+		<div className='flex-1 flex items-center justify-center w-full'>
+			<p>Sorry, something went wrong</p>
+		</div>
+	);
+}
